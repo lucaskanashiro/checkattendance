96dce4f8a77d4f700afba01b23ffdb113e9d7d4e
Lucas Kanashiro <kanashiro@debian.org>
2017-05-07 20:14:51 -0300

    refactor NetworkController

--- app/src/main/java/br/usp/ime/checkattendance/utils/NetworkController.java
+++ app/src/main/java/br/usp/ime/checkattendance/utils/NetworkController.java
@@ -17,192 +17,96 @@ import br.usp.ime.checkattendance.RegisterActivity;
 
 public class NetworkController {
 
-    public void register(String url, final String name, final String nusp, final String passwd,
-                          final Context ctx, final ServerCallback callback) {
+    public void register(String url, String name, String nusp, String passwd, Context context,
+                         ServerCallback callback) {
 
-        StringRequest request = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
+        String params = "nusp=" + nusp + "&pass=" + passwd + "&name=" + name;
+        this.post(url, params, context, callback);
+    }
+
+    public void login(String url, String nusp, String passwd, Context context,
+                      ServerCallback callback) {
+
+        String params = "nusp=" + nusp + "&pass=" + passwd;
+        this.post(url, params, context, callback);
+    }
+
+    public void getAllSeminars(final Context context , final ServerCallback callback) {
+
+        String url = "http://207.38.82.139:8001/seminar";
+        this.get(url, context, callback);
+    }
+
+    public void getAttendedSeminars(String nusp, Context context, ServerCallback callback) {
+
+        String url = "http://207.38.82.139:8001/attendence/listSeminars";
+        String params = "nusp=" + nusp;
+        this.post(url, params, context, callback);
+    }
+
+    public void getStudentData(String nusp, Context context, ServerCallback callback){
+
+        String url = "http://207.38.82.139:8001/student/get/" + nusp;
+        this.get(url, context, callback);
+    }
+
+    public void updateStudentData(String nusp, String name, String passwd, Context context,
+                                  ServerCallback callback) {
+
+        String url = "http://207.38.82.139:8001/student/edit";
+        String params = "nusp=" + nusp + "&name=" + name + "&pass=" + passwd;
+        this.post(url, params, context, callback);
+    }
+
+    private void get(String url, Context context, final ServerCallback callback) {
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>()
+                {
                     @Override
                     public void onResponse(String response) {
-                        Log.d("REGISTER", response +  "\n");
                         callback.onSuccess(response);
                     }
                 }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
                 callback.onError();
             }
-        }
-        ) {
-            String body = "nusp=" + nusp + "&pass=" + passwd + "&name=" + name;
-            @Override
-            public byte[] getBody() throws AuthFailureError {
-                return body.getBytes();
-            }
+        }){
             @Override
             public String getBodyContentType() {
                 return "application/x-www-form-urlencoded; charset=UTF-8";
             }
         };
 
-        RequestQueueSingleton.getInstance(ctx).addToRequestQueue(request);
+        RequestQueueSingleton.getInstance(context).addToRequestQueue(request);
     }
 
-    public void login(String url, final String nusp, final String passwd, Context ctx,
+    private void post(String url, final String params, Context context,
                       final ServerCallback callback) {
 
         StringRequest request = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
+                new Response.Listener<String>()
+                {
                     @Override
                     public void onResponse(String response) {
-                        Log.d("LOGIN", response +  "\n");
                         callback.onSuccess(response);
                     }
                 }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
                 callback.onError();
             }
-        }
-        ) {
-            String body = "nusp=" + nusp + "&pass=" + passwd;
+        }){
+            String body = params;
             @Override
             public byte[] getBody() throws AuthFailureError {
                 return body.getBytes();
             }
             @Override
             public String getBodyContentType() {
-                return "application/x-www-form-urlencoded; charset=UTF-8";
-            }
-        };
-
-        RequestQueueSingleton.getInstance(ctx).addToRequestQueue(request);
-    }
-
-    public void getAllSeminars(final Context context , final ServerCallback callback)
-    {
-        String url = "http://207.38.82.139:8001/seminar";
-
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>()
-        {
-            @Override
-            public void onResponse(String response) {
-                callback.onSuccess(response);
-
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                callback.onError();
-
-            }
-        }){
-            @Override
-            public String getBodyContentType()
-            {
-                return "application/x-www-form-urlencoded; charset=UTF-8";
-            }
-        };
-
-        RequestQueueSingleton.getInstance(context).addToRequestQueue(request);
-    }
-
-    public void getAttendedSeminars(final String nusp, final Context context,
-                                    final ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/attendence/listSeminars";
-
-        StringRequest request = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response) {
-                        callback.onSuccess(response);
-
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                callback.onError();
-
-            }
-        }){
-            String body = "nusp=" + nusp;
-            @Override
-            public byte[] getBody() throws AuthFailureError {
-                return body.getBytes();
-            }
-            @Override
-            public String getBodyContentType()
-            {
-                return "application/x-www-form-urlencoded; charset=UTF-8";
-            }
-        };
-
-        RequestQueueSingleton.getInstance(context).addToRequestQueue(request);
-    }
-
-    public void getStudentData(final String nusp, final Context ctx, final ServerCallback callback){
-        String url = "http://207.38.82.139:8001/student/get/" + nusp;
-
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response) {
-                        callback.onSuccess(response);
-
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                callback.onError();
-
-            }
-        }){
-            @Override
-            public String getBodyContentType()
-            {
-                return "application/x-www-form-urlencoded; charset=UTF-8";
-            }
-        };
-
-        RequestQueueSingleton.getInstance(ctx).addToRequestQueue(request);
-    }
-
-    public void updateStudentData(final String nusp, final String name, final String passwd,
-                                  final Context context, final ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/student/edit";
-
-        StringRequest request = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>()
-                {
-                    @Override
-                    public void onResponse(String response) {
-                        callback.onSuccess(response);
-
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                callback.onError();
-
-            }
-        }){
-            String body = "nusp=" + nusp + "&name=" + name + "&pass=" + passwd;
-            @Override
-            public byte[] getBody() throws AuthFailureError {
-                return body.getBytes();
-            }
-            @Override
-            public String getBodyContentType()
-            {
                 return "application/x-www-form-urlencoded; charset=UTF-8";
             }
         };
