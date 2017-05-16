a71b5d93f582cc7e365e9e24f5cf55e5cd54408f
Lucas Kanashiro <kanashiro@debian.org>
2017-05-16 17:16:47 -0300

    fix domain address in a unique place

--- app/src/main/java/br/usp/ime/checkattendance/utils/NetworkController.java
+++ app/src/main/java/br/usp/ime/checkattendance/utils/NetworkController.java
@@ -22,100 +22,89 @@ import br.usp.ime.checkattendance.RegisterActivity;
 
 public class NetworkController {
 
+    private final String domain = "http://207.38.82.139:8001/";
+
     public void register(String type, String name, String nusp, String passwd, Context context,
                          ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/" + type + "/add";
+        String url = this.domain + type + "/add";
         String params = "nusp=" + nusp + "&pass=" + passwd + "&name=" + name;
         this.post(url, params, context, callback);
     }
 
     public void login(String type, String nusp, String passwd, Context context,
                       ServerCallback callback) {
-        String url = "http://207.38.82.139:8001/login/" + type;
+        String url = this.domain + "login/" + type;
         String params = "nusp=" + nusp + "&pass=" + passwd;
         this.post(url, params, context, callback);
     }
 
     public void getAllSeminars(final Context context , final ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/seminar";
+        String url = this.domain + "seminar";
         this.get(url, context, callback);
     }
 
     public void getStudentData(String nusp, Context context, ServerCallback callback){
-
-        String url = "http://207.38.82.139:8001/student/get/" + nusp;
+        String url = this.domain + "student/get/" + nusp;
         this.get(url, context, callback);
     }
 
     public void getTeacherData(String nusp, Context context, ServerCallback callback){
-
-        String url = "http://207.38.82.139:8001/teacher/get/" + nusp;
+        String url = this.domain + "teacher/get/" + nusp;
         this.get(url, context, callback);
     }
 
     public void updateStudentData(String nusp, String name, String passwd, Context context,
                                   ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/student/edit";
+        String url = this.domain + "student/edit";
         String params = "nusp=" + nusp + "&name=" + name + "&pass=" + passwd;
         this.post(url, params, context, callback);
     }
 
     public void updateTeacherData(String nusp, String name, String passwd, Context context,
                                   ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/teacher/edit";
+        String url = this.domain + "teacher/edit";
         String params = "nusp=" + nusp + "&name=" + name + "&pass=" + passwd;
         this.post(url, params, context, callback);
     }
 
     public void registerSeminar(String name, Context context, ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/seminar/add";
+        String url = this.domain + "seminar/add";
         String params = "name=" + name;
         this.post(url, params, context, callback);
     }
 
     public void getSeminar(String id, Context context, ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/seminar/get/" + id;
+        String url = this.domain + "seminar/get/" + id;
         this.get(url, context, callback);
     }
 
     public void updateSeminar(String id, String name, Context context, ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/seminar/edit";
+        String url = this.domain + "seminar/edit";
         String params = "id=" + id + "&name=" + name;
         this.post(url, params, context, callback);
     }
 
     public void confirmAttendance(String nusp, String seminarId, Context context,
                                          ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/attendence/submit";
+        String url = this.domain + "attendence/submit";
         String params = "nusp=" + nusp + "&seminar_id=" + seminarId;
         this.post(url, params, context, callback);
     }
 
     public void getAttendedSeminars(String nusp, Context context, ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/attendence/listSeminars";
+        String url = this.domain + "attendence/listSeminars";
         String params = "nusp=" + nusp;
         this.post(url, params, context, callback);
     }
 
     public void getAttendees(String seminarId, Context context, ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/attendence/listStudents";
+        String url = this.domain + "attendence/listStudents";
         String params = "seminar_id=" + seminarId;
         this.post(url, params, context, callback);
     }
 
     public void getAllStudents(Context context, ServerCallback callback) {
-
-        String url = "http://207.38.82.139:8001/student";
+        String url = this.domain + "student";
         this.get(url, context, callback);
     }
 
